3262d6cf8200d445df567432a981ffb7003f71fa
Łukasz Gryglicki <lgryglicki@cncf.io>
2023-04-11 07:34:33 +0000

    Update Ortelius config 3
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- allcdf/psql.sh
+++ allcdf/psql.sh
@@ -16,7 +16,7 @@ GHA2DB_PROJECT=allcdf PG_DB=allcdf GHA2DB_LOCAL=1 GHA2DB_MGETC=y structure 2>>er
 ./devel/db.sh psql allcdf -c "create extension if not exists pgcrypto" || exit 1
 if [ ! -z "$MERGE_MODE" ]
 then
-  GHA2DB_INPUT_DBS="spinnaker,tekton,jenkins,jenkinsx,cdevents,ortelius,pyrsia,screwdrivercd,shipwright" GHA2DB_OUTPUT_DB="allcdf" merge_dbs || exit 2
+  GHA2DB_INPUT_DBS="spinnaker,tekton,jenkins,jenkinsx,cdevents,ortelius/azure-infra,ortelius/backstage,ortelius/cli,ortelius/dev-env-setup,ortelius/keptn-config,ortelius/keptn-ortelius-service,ortelius/la-sbom-ledger,ortelius/ms-compitem-crud,ortelius/ms-dep-pkg-cud,ortelius/ms-dep-pkg-r,ortelius/ms-postgres,ortelius/ms-scorecard,ortelius/ms-textfile-crud,ortelius/ms-validate-user,ortelius/ortelius,ortelius/ortelius-charts,ortelius/ortelius-docs,ortelius/ortelius-kubernetes,ortelius/ortelius-python-client,ortelius/ortelius-test-database,ortelius/ortelius-toc,ortelius/ortelius.io,ortelius/outreach,pyrsia,screwdrivercd,shipwright" GHA2DB_OUTPUT_DB="allcdf" merge_dbs || exit 2
 else
   GHA2DB_PROJECT=allcdf PG_DB=allcdf GHA2DB_LOCAL=1 gha2db 2015-01-01 0 today now 'spinnaker,tektoncd,tektoncd-catalog,jenkinsci,jenkins-infra,jenkins-x,jenkins-x-quickstarts,jenkins-x-apps,jenkins-x-charts,jenkins-x-buildpacks,jenkins-x-images,jenkins-x-plugins,knative/build,cdevents,ortelius,pyrsia,screwdriver-cd,shipwright-io,redhat-developer/build,redhat-developer/buildv2,redhat-developer/buildv2-operator' 2>>errors.txt | tee -a run.log || exit 3
   GHA2DB_PROJECT=allcdf PG_DB=allcdf GHA2DB_LOCAL=1 GHA2DB_OLDFMT=1 GHA2DB_EXACT=1 gha2db 2012-07-01 0 2014-12-31 23 'jenkinsci/jenkins,jenkins,jenkins-infra' 2>>errors.txt | tee -a run.log || exit 4
