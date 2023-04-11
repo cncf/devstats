5d672d455a340eda472f14eb06aa70ca24eb8b93
Łukasz Gryglicki <lgryglicki@cncf.io>
2023-04-11 07:32:11 +0000

    Update Ortelius config 2
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- ortelius/psql.sh
+++ ortelius/psql.sh
@@ -13,7 +13,7 @@ fi
 > run.log
 GHA2DB_PROJECT=ortelius PG_DB=ortelius GHA2DB_LOCAL=1 structure 2>>errors.txt | tee -a run.log || exit 1
 ./devel/db.sh psql ortelius -c "create extension if not exists pgcrypto" || exit 1
-GHA2DB_PROJECT=ortelius PG_DB=ortelius GHA2DB_LOCAL=1 gha2db 2017-03-06 0 today now "ortelius" 2>>errors.txt | tee -a run.log || exit 2
+GHA2DB_PROJECT=ortelius PG_DB=ortelius GHA2DB_LOCAL=1 gha2db 2017-03-06 0 today now "ortelius,ortelius/azure-infra,ortelius/backstage,ortelius/cli,ortelius/dev-env-setup,ortelius/keptn-config,ortelius/keptn-ortelius-service,ortelius/la-sbom-ledger,ortelius/ms-compitem-crud,ortelius/ms-dep-pkg-cud,ortelius/ms-dep-pkg-r,ortelius/ms-postgres,ortelius/ms-scorecard,ortelius/ms-textfile-crud,ortelius/ms-validate-user,ortelius/ortelius,ortelius/ortelius-charts,ortelius/ortelius-docs,ortelius/ortelius-kubernetes,ortelius/ortelius-python-client,ortelius/ortelius-test-database,ortelius/ortelius-toc,ortelius/ortelius.io,ortelius/outreach" 2>>errors.txt | tee -a run.log || exit 2
 GHA2DB_PROJECT=ortelius PG_DB=ortelius GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 structure 2>>errors.txt | tee -a run.log || exit 4
 GHA2DB_PROJECT=ortelius PG_DB=ortelius ./shared/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 5
 GHA2DB_PROJECT=ortelius PG_DB=ortelius ./shared/import_affs.sh 2>>errors.txt | tee -a run.log || exit 6
