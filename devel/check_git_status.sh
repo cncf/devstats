2a192c1452c4bb7f69c40a1a35680bc7db206e43
Łukasz Gryglicki <lgryglicki@cncf.io>
2023-05-12 08:25:06 +0000

    Update checker script
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- devel/check_git_status.sh
+++ devel/check_git_status.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 cd ..
-for f in devstats devstatscode devstats-docker-images devstats-docker-lf devstats-example devstats-helm devstats-helm-lf devstats-k8s-lf devstats-reports devstats-helm-example devstats-helm-graphql devstats-kubernetes-dashboard 
+for f in devstats devstatscode devstats-docker-images devstats-docker-lf devstats-example devstats-helm devstats-helm-lf devstats-k8s-lf devstats-reports devstats-helm-example devstats-helm-graphql devstats-kubernetes-dashboard devstats-landscape-sync
 do
-  cd $f
+  if [ ! -d "${f}" ]
+  then
+    git clone "https://github.com/cncf/${f}" || exit 1
+  fi
+  cd "${f}"
   echo ">>>>> status: $f <<<<<"
   git status
   cd ..
