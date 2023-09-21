caadb6f8deff0e21a127b55202dfecf982fa6391
Łukasz Gryglicki <lgryglicki@cncf.io>
2023-09-21 05:35:19 +0000

    Adding support for gha_repo_groups which allows one repo to belong to multiple repo groups 2
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

---
+++ metrics/cncf/countries_cum.sql
@@ -0,0 +1,95 @@
+select
+  concat(inn.type, ';', inn.country_name, '`', inn.repo_group, ';contributors,contributions,users,events,committers,commits,prcreators,prs,issuecreators,issues,commenters,comments,reviewers,reviews,watchers,watches,forkers,forks') as name,
+  inn.contributors,
+  inn.contributions,
+  inn.users,
+  inn.events,
+  inn.committers,
+  inn.commits,
+  inn.prcreators,
+  inn.prs,
+  inn.issuecreators,
+  inn.issues,
+  inn.commenters,
+  inn.comments,
+  inn.reviewers,
+  inn.reviews,
+  inn.watchers,
+  inn.watches,
+  inn.forkers,
+  inn.forks
+from (
+  select 'countriescum' as type,
+    a.country_name,
+    'all' as repo_group,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type in ('IssuesEvent', 'PullRequestEvent', 'PushEvent', 'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent', 'PullRequestReviewEvent') when true then e.actor_id end)))) as contributors,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type in ('IssuesEvent', 'PullRequestEvent', 'PushEvent', 'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent', 'PullRequestReviewEvent') when true then e.id end)))) as contributions,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(e.actor_id)))) as users,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(e.id)))) as events,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type = 'PushEvent' when true then e.actor_id end)))) as committers,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type = 'PushEvent' when true then e.id end)))) as commits,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type = 'PullRequestEvent' when true then e.actor_id end)))) as prcreators,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type = 'PullRequestEvent' when true then e.id end)))) as prs,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type = 'IssuesEvent' when true then e.actor_id end)))) as issuecreators,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type = 'IssuesEvent' when true then e.id end)))) as issues,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type in ('CommitCommentEvent', 'IssueCommentEvent') when true then e.actor_id end)))) as commenters,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type in ('CommitCommentEvent', 'IssueCommentEvent') when true then e.id end)))) as comments,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type in ('PullRequestReviewCommentEvent', 'PullRequestReviewEvent') when true then e.actor_id end)))) as reviewers,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type in ('PullRequestReviewCommentEvent', 'PullRequestReviewEvent') when true then e.id end)))) as reviews,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type = 'WatchEvent' when true then e.actor_id end)))) as watchers,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type = 'WatchEvent' when true then e.id end)))) as watches,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type = 'ForkEvent' when true then e.actor_id end)))) as forkers,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type = 'ForkEvent' when true then e.id end)))) as forks
+  from
+    gha_events e,
+    gha_actors a
+  where
+    (lower(a.login) {{exclude_bots}})
+    and a.id = e.actor_id
+    and a.country_name is not null
+    and a.country_name != ''
+    and e.created_at < '{{to}}'
+  group by
+    a.country_name
+  union select 'countriescum' as type,
+    a.country_name,
+    r.repo_group as repo_group,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type in ('IssuesEvent', 'PullRequestEvent', 'PushEvent', 'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent', 'PullRequestReviewEvent') when true then e.actor_id end)))) as contributors,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type in ('IssuesEvent', 'PullRequestEvent', 'PushEvent', 'CommitCommentEvent', 'IssueCommentEvent', 'PullRequestReviewCommentEvent', 'PullRequestReviewEvent') when true then e.id end)))) as contributions,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(e.actor_id)))) as users,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(e.id)))) as events,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type = 'PushEvent' when true then e.actor_id end)))) as committers,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type = 'PushEvent' when true then e.id end)))) as commits,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type = 'PullRequestEvent' when true then e.actor_id end)))) as prcreators,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type = 'PullRequestEvent' when true then e.id end)))) as prs,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type = 'IssuesEvent' when true then e.actor_id end)))) as issuecreators,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type = 'IssuesEvent' when true then e.id end)))) as issues,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type in ('CommitCommentEvent', 'IssueCommentEvent') when true then e.actor_id end)))) as commenters,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type in ('CommitCommentEvent', 'IssueCommentEvent') when true then e.id end)))) as comments,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type in ('PullRequestReviewCommentEvent', 'PullRequestReviewEvent') when true then e.actor_id end)))) as reviewers,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type in ('PullRequestReviewCommentEvent', 'PullRequestReviewEvent') when true then e.id end)))) as reviews,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type = 'WatchEvent' when true then e.actor_id end)))) as watchers,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type = 'WatchEvent' when true then e.id end)))) as watches,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type = 'ForkEvent' when true then e.actor_id end)))) as forkers,
+    round(hll_cardinality(hll_add_agg(hll_hash_bigint(case e.type = 'ForkEvent' when true then e.id end)))) as forks
+  from
+    gha_repo_groups r,
+    gha_actors a,
+    gha_events e
+  where
+    r.id = e.repo_id
+    and r.name = e.dup_repo_name
+    and (lower(a.login) {{exclude_bots}})
+    and a.id = e.actor_id
+    and a.country_name is not null
+    and a.country_name != ''
+    and e.created_at < '{{to}}'
+  group by
+    a.country_name,
+    r.repo_group
+) inn
+where
+  inn.repo_group is not null 
+order by
+  name
+;
--- util_sql/gha_repo_groups.sql
+++ util_sql/gha_repo_groups.sql
@@ -6,11 +6,16 @@ CREATE TABLE public.gha_repo_groups (
     org_login character varying(100),
     alias character varying(160)
 );
+
 ALTER TABLE public.gha_repo_groups OWNER TO gha_admin;
+
 ALTER TABLE ONLY public.gha_repo_groups ADD CONSTRAINT gha_repo_groups_pkey PRIMARY KEY (id, name, repo_group);
+
 CREATE INDEX repo_groups_alias_idx ON public.gha_repo_groups USING btree (alias);
 CREATE INDEX repo_groups_id_idx ON public.gha_repo_groups USING btree (id);
 CREATE INDEX repo_groups_name_idx ON public.gha_repo_groups USING btree (name);
 CREATE INDEX repo_groups_org_id_idx ON public.gha_repo_groups USING btree (org_id);
 CREATE INDEX repo_groups_org_login_idx ON public.gha_repo_groups USING btree (org_login);
 CREATE INDEX repo_groups_repo_group_idx ON public.gha_repo_groups USING btree (repo_group);
+
+insert into gha_repo_groups(id, name, alias, repo_group, org_id, org_login) select id, name, alias, repo_group, org_id, org_login from gha_repos on conflict do nothing;
