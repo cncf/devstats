6c11f5fbaa1ecb3c79fad3d658139b1aba52d293
Łukasz Gryglicki <lgryglicki@cncf.io>
2023-12-29 05:50:16 +0000

    New bots
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- structure.sql
+++ structure.sql
@@ -1243,6 +1243,9 @@ COPY public.gha_assets (id, event_id, name, label, uploader_id, content_type, st
 --
 
 COPY public.gha_bot_logins (pattern) FROM stdin;
+megaeasex
+kuasar-io-dev
+startxfr
 opentelemetrybot
 invalid-email-address
 fluxcdbot
