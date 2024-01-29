ed18d94b5acdf567698f080122344f0b58dc4e45
Łukasz Gryglicki <lgryglicki@cncf.io>
2024-01-29 05:27:00 +0000

    Update company commits data metric
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- metrics/shared/company_commits_data.sql
+++ metrics/shared/company_commits_data.sql
@@ -12,7 +12,7 @@ from (
     c.sha as commit_sha,
     regexp_replace(substring(c.message for 80), '[\n\r$$$]+', ' ', 'g') as commit_msg,
     c.dup_created_at as commit_date
-  from 
+  from
     gha_actors_affiliations af,
     gha_repo_groups r,
     gha_commits c
@@ -32,6 +32,72 @@ from (
   group by
     af.company_name,
     c.dup_author_login,
+    c.dup_repo_name,
+    c.sha,
+    c.message,
+    c.dup_created_at,
+    r.repo_group
+  union select distinct af.company_name as company,
+    r.repo_group as repo_group,
+    c.dup_committer_login as author,
+    string_agg(distinct an.name, ', ') as author_names,
+    c.dup_repo_name as commit_repo,
+    c.sha as commit_sha,
+    regexp_replace(substring(c.message for 80), '[\n\r$$$]+', ' ', 'g') as commit_msg,
+    c.dup_created_at as commit_date
+  from
+    gha_actors_affiliations af,
+    gha_repo_groups r,
+    gha_commits c
+  left join
+    gha_actors_names an
+  on
+    c.committer_id = an.actor_id
+  where
+    c.dup_repo_id = r.id
+    and c.dup_repo_name = r.name
+    and c.committer_id = af.actor_id
+    and af.dt_from <= c.dup_created_at
+    and af.dt_to > c.dup_created_at
+    and c.dup_created_at >= '{{from}}'
+    and c.dup_created_at < '{{to}}'
+    and (lower(c.dup_committer_login) {{exclude_bots}})
+  group by
+    af.company_name,
+    c.dup_committer_login,
+    c.dup_repo_name,
+    c.sha,
+    c.message,
+    c.dup_created_at,
+    r.repo_group
+  union select distinct af.company_name as company,
+    r.repo_group as repo_group,
+    c.dup_actor_login as author,
+    string_agg(distinct an.name, ', ') as author_names,
+    c.dup_repo_name as commit_repo,
+    c.sha as commit_sha,
+    regexp_replace(substring(c.message for 80), '[\n\r$$$]+', ' ', 'g') as commit_msg,
+    c.dup_created_at as commit_date
+  from
+    gha_actors_affiliations af,
+    gha_repo_groups r,
+    gha_commits c
+  left join
+    gha_actors_names an
+  on
+    c.dup_actor_id = an.actor_id
+  where
+    c.dup_repo_id = r.id
+    and c.dup_repo_name = r.name
+    and c.dup_actor_id = af.actor_id
+    and af.dt_from <= c.dup_created_at
+    and af.dt_to > c.dup_created_at
+    and c.dup_created_at >= '{{from}}'
+    and c.dup_created_at < '{{to}}'
+    and (lower(c.dup_actor_login) {{exclude_bots}})
+  group by
+    af.company_name,
+    c.dup_actor_login,
     c.dup_repo_name,
     c.sha,
     c.message,
