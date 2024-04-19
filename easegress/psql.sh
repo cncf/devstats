ecb87fbd37ae3780fbcbc81a223c61298e78947f
Łukasz Gryglicki <lgryglicki@cncf.io>
2024-04-19 06:11:12 +0000

    Easegress moved to a new GitHub org megaease -> easegress-io - wip 3
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- easegress/psql.sh
+++ easegress/psql.sh
@@ -15,7 +15,7 @@ GHA2DB_PROJECT=easegress PG_DB=easegress GHA2DB_LOCAL=1 structure 2>>errors.txt 
 ./devel/db.sh psql easegress -c "create extension if not exists pgcrypto" || exit 1
 ./devel/db.sh psql easegress -c "create extension if not exists hll" || exit 1
 ./devel/ro_user_grants.sh easegress || exit 2
-GHA2DB_PROJECT=easegress PG_DB=easegress GHA2DB_LOCAL=1 gha2db 2017-06-07 0 today now 'megaease' 2>>errors.txt | tee -a run.log || exit 3
+GHA2DB_PROJECT=easegress PG_DB=easegress GHA2DB_LOCAL=1 gha2db 2017-06-07 0 today now 'megaease,easegress-io' 2>>errors.txt | tee -a run.log || exit 3
 GHA2DB_PROJECT=easegress PG_DB=easegress GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 structure 2>>errors.txt | tee -a run.log || exit 5
 GHA2DB_PROJECT=easegress PG_DB=easegress ./shared/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 6
 GHA2DB_PROJECT=easegress PG_DB=easegress ./shared/setup_scripts.sh 2>>errors.txt | tee -a run.log || exit 7
