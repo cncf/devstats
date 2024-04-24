5572812ab17e41e984d85c10e8a1c6526afbdf62
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2024-04-24 08:01:06 +0000

    Added psql cons analyze tool 2
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_rb/analyze.rb
+++ util_rb/analyze.rb
@@ -8,20 +8,35 @@ if ARGV.length < 2
 end
 fn = ARGV[0]
 metric = ARGV[1]
+top = ENV['TOP']
+top = top.to_i unless top.nil?
 
 data = []
 File.readlines(fn).each_with_index do |line, i|
-  m = line.match /.*#{metric}=(\d+\.\d+)\.\.(\d+\.\d+)*/
-  next unless m
-  t = m[2].to_f - m[1].to_f
-  next if t == 0.0
-  ti = m[2].to_i - m[1].to_i
-  s = m[1] + ".." + m[2]
-  next if ti == 0 || t < 1.0
-  s = m[1] + ".." + m[2]
-  data << [t, s, i, line]
+  m = line.match /.*#{metric}=(\d+i(\.\d+)?)\.\.(\d+(\.\d+)?).*/
+  if m
+    t = m[2].to_f - m[1].to_f
+    next if t == 0.0
+    ti = m[2].to_i - m[1].to_i
+    next if ti == 0 || t < 1.0
+    s = m[1] + ".." + m[2]
+    data << [t, s, i, line]
+    next
+  end
+  m = line.match /.*#{metric}=(\d+(\.\d+)?).*/
+  if m
+    t = m[1].to_f
+    next if t == 0.0
+    ti = m[1].to_i
+    next if ti == 0 || t < 1.0
+    s = m[1]
+    data << [t, s, i, line]
+    next
+  end
 end
+
 data = data.sort_by { |row| -row[0] }
-data.each do |row|
+data.each_with_index do |row, i|
+  break if top > 0 and i > top
   puts "#{metric}: ##{row[2]}) #{row[0]}: #{row[3]}"
 end
