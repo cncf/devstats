d5876b89df97f2467376cc190c88733da93fdb20
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2024-04-24 08:44:58 +0000

    Added psql cons analyze tool 3
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_rb/analyze.rb
+++ util_rb/analyze.rb
@@ -11,32 +11,37 @@ metric = ARGV[1]
 top = ENV['TOP']
 top = top.to_i unless top.nil?
 
-data = []
-File.readlines(fn).each_with_index do |line, i|
-  m = line.match /.*#{metric}=(\d+\.\d+|\d+)\.\.(\d+\.\d+|\d+).*/
-  if m
-    t = m[2].to_f - m[1].to_f
-    next if t == 0.0
-    ti = m[2].to_i - m[1].to_i
-    next if ti == 0 || t < 1.0
-    s = m[1] + ".." + m[2]
-    data << [t, s, i, line]
-    next
+metric_ary = metric.split(',')
+metric_ary.each do |metric|
+  data = []
+  File.readlines(fn).each_with_index do |line, i|
+    m = line.match /.*#{metric}=(\d+\.\d+|\d+)\.\.(\d+\.\d+|\d+).*/
+    if m
+      t = m[2].to_f - m[1].to_f
+      next if t == 0.0
+      ti = m[2].to_i - m[1].to_i
+      next if ti == 0 || t < 1.0
+      s = m[1] + ".." + m[2]
+      data << [t, s, i, line]
+      next
+    end
+    m = line.match /.*#{metric}=(\d+\.\d+|\d+).*/
+    if m
+      t = m[1].to_f
+      next if t == 0.0
+      ti = m[1].to_i
+      next if ti == 0 || t < 1.0
+      s = m[1]
+      data << [t, s, i, line]
+      next
+    end
   end
-  m = line.match /.*#{metric}=(\d+\.\d+|\d+).*/
-  if m
-    t = m[1].to_f
-    next if t == 0.0
-    ti = m[1].to_i
-    next if ti == 0 || t < 1.0
-    s = m[1]
-    data << [t, s, i, line]
-    next
+  
+  puts "#{metric}:"
+  data = data.sort_by { |row| -row[0] }
+  data.each_with_index do |row, i|
+    break if top > 0 and i >= top
+    v = sprintf "%.2f", row[0]
+    puts "#{i+1}) #{metric}: ##{row[2]}) #{v}:\t\t#{row[3].strip}"
   end
-end
-
-data = data.sort_by { |row| -row[0] }
-data.each_with_index do |row, i|
-  break if top > 0 and i > top
-  puts "#{metric}: ##{row[2]}) #{row[0]}: #{row[3]}"
 end
