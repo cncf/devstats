4f23dc1e934d44b1ed07d4b8af937da19701c9f2
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2024-04-24 06:37:53 +0000

    Add runq.sh script for K8s node usage
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ util_sh/runq.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ ! -z "$VERBOSE" ]
+then
+  echo "GHA2DB_ABSOLUTE=1 GHA2DB_DEBUG=-1 GHA2DB_DRY_RUN=1 /data/go/src/github.com/cncf/devstatscode/runq \"${1}\" {{exclude_bots}} \"cat /data/go/src/github.com/cncf/devstats/util_sql/exclude_bots.sql\" \"${@:2}\""
+fi
+GHA2DB_ABSOLUTE=1 GHA2DB_DEBUG=-1 GHA2DB_DRY_RUN=1 /data/go/src/github.com/cncf/devstatscode/runq "${1}" {{exclude_bots}} "$(cat /data/go/src/github.com/cncf/devstats/util_sql/exclude_bots.sql)" "${@:2}"
---
+++ util_sql/invalid_email.sql
@@ -0,0 +1,1 @@
+delete from shdev where name like 'invalid-email-address$$$%';
