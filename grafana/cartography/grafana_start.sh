1c75e7c9f287670f35acfcbda9fb70b2dad3a4ed
Łukasz Gryglicki <lgryglicki@cncf.io>
2024-09-06 10:15:34 +0000

    Adding 6 new CNCF Sandbox projects 9/6/24 - wip 30
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- grafana/cartography/grafana_start.sh
+++ grafana/cartography/grafana_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /usr/share/grafana.carthography
-grafana-server -config /etc/grafana.carthography/grafana.ini cfg:default.paths.data=/var/lib/grafana.carthography 1>/var/log/grafana.carthography.log 2>&1
+cd /usr/share/grafana.cartography
+grafana-server -config /etc/grafana.cartography/grafana.ini cfg:default.paths.data=/var/lib/grafana.cartography 1>/var/log/grafana.cartography.log 2>&1
