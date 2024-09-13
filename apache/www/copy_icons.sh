7dc599450cece1cdce398aebfa3623a43c1b0eaf
Łukasz Gryglicki <lgryglicki@cncf.io>
2024-09-13 04:15:06 +0000

    Update artwork
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- apache/www/copy_icons.sh
+++ apache/www/copy_icons.sh
@@ -118,7 +118,7 @@ do
     icon="bank-vaults"
   fi
   # TODO: remove when we have icons
-  if ( [ "$icon" = "flatcar" ] || [ "$icon" = "loxilb" ] || [ "$icon" = "cartography" ] || [ "$icon" = "ratify" ] || [ "$icon" = "shipwrightcncf" ] || [ "$icon" = "atlantis" ] || [ "$icon" = "stacker" ] || [ "$icon" = "kanister" ] || [ "$icon" = "kpt" ] || [ "$icon" = "kubeclipper" ] || [ "$icon" = "sops" ] || [ "$icon" = "lima" ] || [ "$icon" = "sealer" ] || [ "$icon" = "openelb" ] || [ "$icon" = "vscodek8stools" ] || [ "$icon" = "kubevip" ] || [ "$icon" = "cnigenie" ] || [ "$icon" = "devspace" ] || [ "$icon" = "ko" ] || [ "$icon" = "contrib" ] || [ "$icon" = "sam" ] || [ "$icon" = "azf" ] || [ "$icon" = "riff" ] || [ "$icon" = "fn" ] || [ "$icon" = "openwhisk" ] || [ "$icon" = "openfaas" ] || [ "$icon" = "cii" ] )
+  if ( [ "$icon" = "flatcar" ] || [ "$icon" = "cartography" ] || [ "$icon" = "ratify" ] || [ "$icon" = "shipwrightcncf" ] || [ "$icon" = "atlantis" ] || [ "$icon" = "stacker" ] || [ "$icon" = "kanister" ] || [ "$icon" = "kpt" ] || [ "$icon" = "kubeclipper" ] || [ "$icon" = "sops" ] || [ "$icon" = "lima" ] || [ "$icon" = "sealer" ] || [ "$icon" = "openelb" ] || [ "$icon" = "vscodek8stools" ] || [ "$icon" = "kubevip" ] || [ "$icon" = "cnigenie" ] || [ "$icon" = "devspace" ] || [ "$icon" = "ko" ] || [ "$icon" = "contrib" ] || [ "$icon" = "sam" ] || [ "$icon" = "azf" ] || [ "$icon" = "riff" ] || [ "$icon" = "fn" ] || [ "$icon" = "openwhisk" ] || [ "$icon" = "openfaas" ] || [ "$icon" = "cii" ] )
   then
     icon="cncf"
   fi
