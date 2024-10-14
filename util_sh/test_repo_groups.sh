082ab9cdf62b0f8bdc74ee4c1c6752ed91f5708b
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2024-10-14 06:14:53 +0000

    Update script
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- util_sh/test_repo_groups.sh
+++ util_sh/test_repo_groups.sh
@@ -13,8 +13,10 @@ then
 fi
 kubectl exec -itn devstats-prod devstats-postgres-$N -- pg_dump $DB --table gha_repos > "${DB}_gha_repos.sql"
 kubectl exec -itn devstats-prod devstats-postgres-$N -- pg_dump $DB --table gha_repo_groups > "${DB}_gha_repo_groups.sql"
+kubectl exec -itn devstats-prod devstats-postgres-$N -- pg_dump $DB --table gha_events > "${DB}_gha_events.sql"
 kubectl exec -itn devstats-prod devstats-postgres-$N -- dropdb -f --if-exists "${DB}_tmp" 1>/dev/null 2>/dev/null
 kubectl exec -itn devstats-prod devstats-postgres-$N -- createdb "${DB}_tmp"
 kubectl exec -in devstats-prod devstats-postgres-$N -- psql "${DB}_tmp" < "${DB}_gha_repos.sql"
 kubectl exec -in devstats-prod devstats-postgres-$N -- psql "${DB}_tmp" < "${DB}_gha_repo_groups.sql"
-rm -f "${DB}_gha_repos.sql" "${DB}_gha_repo_groups.sql"
+kubectl exec -in devstats-prod devstats-postgres-$N -- psql "${DB}_tmp" < "${DB}_gha_events.sql"
+rm -f "${DB}_gha_repos.sql" "${DB}_gha_repo_groups.sql" "${DB}_gha_events.sql"
