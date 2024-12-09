24e627f777df29c81f306e48e00aa8a849e24b3f
Łukasz Gryglicki <lgryglicki@cncf.io>
2024-12-09 07:17:05 +0000

    Update kaito artwork
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- grafana/create_images.sh
+++ grafana/create_images.sh
@@ -128,7 +128,7 @@ do
     icon="shipwright"
   fi
   # TODO: remove when we have icons
-  if ( [ "$icon" = "youki" ] || [ "$icon" = "kaito" ] || [ "$icon" = "sermant" ] || [ "$icon" = "kmesh" ] || [ "$icon" = "ovnkubernetes" ] || [ "$icon" = "kanister" ] || [ "$icon" = "kubeclipper" ] || [ "$icon" = "sealer" ] || [ "$icon" = "openelb" ] || [ "$icon" = "flatcar" ] || [ "$icon" = "cartography" ] || [ "$icon" = "lima" ] || [ "$icon" = "vscodek8stools" ] || [ "$icon" = "kubevip" ] || [ "$icon" = "cnigenie" ] || [ "$icon" = "contrib" ] || [ "$icon" = "sam" ] || [ "$icon" = "azf" ] || [ "$icon" = "riff" ] || [ "$icon" = "fn" ] || [ "$icon" = "openwhisk" ] || [ "$icon" = "openfaas" ] || [ "$icon" = "cii" ] )
+  if ( [ "$icon" = "youki" ] || [ "$icon" = "sermant" ] || [ "$icon" = "kmesh" ] || [ "$icon" = "ovnkubernetes" ] || [ "$icon" = "kanister" ] || [ "$icon" = "kubeclipper" ] || [ "$icon" = "sealer" ] || [ "$icon" = "openelb" ] || [ "$icon" = "flatcar" ] || [ "$icon" = "cartography" ] || [ "$icon" = "lima" ] || [ "$icon" = "vscodek8stools" ] || [ "$icon" = "kubevip" ] || [ "$icon" = "cnigenie" ] || [ "$icon" = "contrib" ] || [ "$icon" = "sam" ] || [ "$icon" = "azf" ] || [ "$icon" = "riff" ] || [ "$icon" = "fn" ] || [ "$icon" = "openwhisk" ] || [ "$icon" = "openfaas" ] || [ "$icon" = "cii" ] )
   then
     icon="cncf"
   fi
