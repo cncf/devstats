939eae792fc4c48a6cdc5454facf3202ccd43803
Lukasz Gryglicki <lgryglicki@cncf.io>
2025-04-11 07:07:22 +0000

    Update projects config 3
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>

--- slimfaas/psql.sh
+++ slimfaas/psql.sh
@@ -15,7 +15,7 @@ GHA2DB_PROJECT=slimfaas PG_DB=slimfaas GHA2DB_LOCAL=1 structure 2>>errors.txt | 
 ./devel/db.sh psql slimfaas -c "create extension if not exists pgcrypto" || exit 1
 ./devel/db.sh psql slimfaas -c "create extension if not exists hll" || exit 1
 ./devel/ro_user_grants.sh slimfaas || exit 2
-GHA2DB_PROJECT=slimfaas PG_DB=slimfaas GHA2DB_LOCAL=1 gha2db 2023-06-21 0 today now 'AxaFrance/SlimFaas' 2>>errors.txt | tee -a run.log || exit 3
+GHA2DB_PROJECT=slimfaas PG_DB=slimfaas GHA2DB_LOCAL=1 gha2db 2023-06-21 0 today now 'SlimPlanet,AxaFrance/SlimFaaS' 2>>errors.txt | tee -a run.log || exit 3
 GHA2DB_PROJECT=slimfaas PG_DB=slimfaas GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 structure 2>>errors.txt | tee -a run.log || exit 5
 GHA2DB_PROJECT=slimfaas PG_DB=slimfaas ./shared/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 6
 GHA2DB_PROJECT=slimfaas PG_DB=slimfaas ./shared/setup_scripts.sh 2>>errors.txt | tee -a run.log || exit 7
