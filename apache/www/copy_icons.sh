902720d9aa3d3da3e90a03777ea7791ea620d967
Lukasz Gryglicki <lgryglicki@cncf.io>
2025-04-28 06:22:12 +0000

    Update artwork scripts
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>

--- apache/www/copy_icons.sh
+++ apache/www/copy_icons.sh
@@ -121,8 +121,12 @@ do
   then
     icon="shipwright"
   fi
+  if [ "$icon" = "tratteria" ]
+  then
+    icon="tokenetes"
+  fi
   # TODO: remove when we have icons
-  if ( [ "$icon" = "interlink" ] || [ "$icon" = "kgateway" ] || [ "$icon" = "kitops" ] || [ "$icon" = "tratteria" ] || [ "$icon" = "container2wasm" ] || [ "$icon" = "k0s" ] || [ "$icon" = "runmenotebooks" ] || [ "$icon" = "kubefleet" ] || [ "$icon" = "podmandesktop" ] || [ "$icon" = "podmancontainertools" ] || [ "$icon" = "bootc" ] || [ "$icon" = "composefs" ] || [ "$icon" = "youki" ] || [ "$icon" = "ovnkubernetes" ] || [ "$icon" = "atlantis" ] || [ "$icon" = "kanister" ] || [ "$icon" = "kubeclipper" ] || [ "$icon" = "sealer" ] || [ "$icon" = "openelb" ] || [ "$icon" = "cartography" ] || [ "$icon" = "lima" ] || [ "$icon" = "vscodek8stools" ] || [ "$icon" = "kubevip" ] || [ "$icon" = "cnigenie" ] || [ "$icon" = "contrib" ] || [ "$icon" = "sam" ] || [ "$icon" = "azf" ] || [ "$icon" = "riff" ] || [ "$icon" = "fn" ] || [ "$icon" = "openwhisk" ] || [ "$icon" = "openfaas" ] || [ "$icon" = "cii" ] )
+  if ( [ "$icon" = "interlink" ] || [ "$icon" = "kgateway" ] || [ "$icon" = "kitops" ] || [ "$icon" = "container2wasm" ] || [ "$icon" = "k0s" ] || [ "$icon" = "runmenotebooks" ] || [ "$icon" = "kubefleet" ] || [ "$icon" = "podmandesktop" ] || [ "$icon" = "podmancontainertools" ] || [ "$icon" = "bootc" ] || [ "$icon" = "composefs" ] || [ "$icon" = "youki" ] || [ "$icon" = "ovnkubernetes" ] || [ "$icon" = "atlantis" ] || [ "$icon" = "kanister" ] || [ "$icon" = "kubeclipper" ] || [ "$icon" = "sealer" ] || [ "$icon" = "openelb" ] || [ "$icon" = "cartography" ] || [ "$icon" = "lima" ] || [ "$icon" = "vscodek8stools" ] || [ "$icon" = "kubevip" ] || [ "$icon" = "cnigenie" ] || [ "$icon" = "contrib" ] || [ "$icon" = "sam" ] || [ "$icon" = "azf" ] || [ "$icon" = "riff" ] || [ "$icon" = "fn" ] || [ "$icon" = "openwhisk" ] || [ "$icon" = "openfaas" ] || [ "$icon" = "cii" ] )
   then
     icon="cncf"
   fi
