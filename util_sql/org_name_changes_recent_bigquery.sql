024267b361ba5dea4f3df10e40981bcd43d23c2b
Łukasz Gryglicki <lgryglicki@cncf.io>
2025-06-17 08:25:21 +0200

    Add recent trackers scripts
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

---
+++ util_sql/org_name_changes_recent_bigquery.sql
@@ -0,0 +1,41 @@
+#legacySQL
+select
+  org.id as org_id,
+  org.login as org,
+  repo.name as repo,
+  repo.id as rid,
+  min(created_at) as date_from,
+  max(created_at) as date_to
+from
+--  [githubarchive:month.202501],
+  [githubarchive:month.202505],
+  [githubarchive:month.202504],
+  [githubarchive:month.202503],
+  [githubarchive:month.202502],
+  [githubarchive:month.202501]
+where
+  org.id = (
+    select
+      org.id
+    from
+--      [githubarchive:month.202501],
+      [githubarchive:month.202505],
+      [githubarchive:month.202504],
+      [githubarchive:month.202503],
+      [githubarchive:month.202502],
+      [githubarchive:month.202501]
+    where
+      org.login = '{{org}}'
+    group by
+      org.id,
+      created_at
+    order by
+      created_at desc
+    limit
+      1
+  )
+group by
+  org_id, org, repo, rid
+order by
+  date_from
+;
