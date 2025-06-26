18e81e366d24b57f262dc046d0ecab3d46c27e6b
Łukasz Gryglicki <lgryglicki@cncf.io>
2025-06-26 10:33:48 +0200

    Added util scripts
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

---
+++ util_sh/recreate_tsdb_tables.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+if [ -z "$1" ]
+then
+  echo "$0: you need to specify which DB as a 1st argument, for example 'jenkins'"
+  exit 1
+fi
+export KENV="devstats-prod"
+if [ ! -z "$TEST" ]
+then
+  export KENV="devstats-test"
+fi
+
+if [ -z "$N" ]
+then
+  export N='0'
+fi
+
+tmpfile=$(mktemp /tmp/recreate_sql.XXXXXX.sql)
+trap "rm -f \"$tmpfile\"" EXIT
+> "$tmpfile"
+for t in $(kubectl exec -in "$KENV" "devstats-postgres-$N" -- psql -d "$1" -At -c "select tablename from pg_tables where schemaname = 'public' AND tablename LIKE 's%'")
+do
+    echo "$1:$t"
+    echo "create table \"${t}_tmp\" (like \"$t\" including all);" >> "$tmpfile"
+    echo "insert into \"${t}_tmp\" select * from \"$t\";" >> "$tmpfile"
+    echo "drop table \"$t\";" >> "$tmpfile"
+    echo "alter table \"${t}_tmp\" rename to \"$t\";" >> "$tmpfile"
+done
+
+cat "$tmpfile"
+echo "kubectl exec -i -n \"$KENV\" \"devstats-postgres-$N\" -- psql -d \"$1\" < \"$tmpfile\""
+kubectl exec -i -n "$KENV" "devstats-postgres-$N" -- psql -d "$1" < "$tmpfile"
