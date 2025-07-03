819fd180fb6a14c797c9940492451dbb69ccafa9
Łukasz Gryglicki <lgryglicki@cncf.io>
2025-07-03 10:50:57 +0200

    Added util script
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

---
+++ devel/kubectl-db.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# DBDEBUG=1 - verbose operations
+if [ -z "$1" ]
+then
+  echo "$0 you need to specify at least one argument"
+  exit 1
+fi
+if [ -z "${STAGE}" ]
+then
+  STAGE=prod
+fi
+if [ -z "${N}" ]
+then
+  N=0
+fi
+cmd=${1}
+shift
+quoted_args=$(printf '%q ' "$@")
+if [ ! -z "$DBDEBUG" ]
+then
+  echo "kubectl exec -itn 'devstats-${STAGE}' 'devstats-postgres-${N}' -- bash -c \"${cmd} ${quoted_args}\""
+fi
+kubectl exec -itn "devstats-${STAGE}" "devstats-postgres-${N}" -- bash -c "${cmd} ${quoted_args}"
