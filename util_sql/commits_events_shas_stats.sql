0e4befab98d0ec23bb1f7faf11a8bab88a98c5cd
Lukasz Gryglicki <lgryglicki@cncf.io>
2026-02-18 06:52:04 +0100

    Update util SQL
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>

--- util_sql/commits_events_shas_stats.sql
+++ util_sql/commits_events_shas_stats.sql
@@ -1,3 +1,4 @@
-select count(*), count(distinct sha), count(distinct event_id) from gha_commits;
-select sha, count(distinct event_id) as cnt from gha_commits group by 1 order by 2 desc limit 10;
-select event_id, count(distinct sha) as cnt from gha_commits group by 1 order by 2 desc limit 10;
+select count(*) as count, count(distinct sha) as dist_shas, count(distinct event_id) as dist_events from gha_commits;
+select origin, count(distinct sha) as dist_shas, count(distinct event_id) as dist_events, count(*) as count from gha_commits group by 1;
+select sha, count(distinct event_id) as dist_events from gha_commits group by 1 order by 2 desc limit 10;
+select event_id, count(distinct sha) as dist_shas from gha_commits group by 1 order by 2 desc limit 10;
