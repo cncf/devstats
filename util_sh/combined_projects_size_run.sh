310aec42020c12e9c75637a7bd47f6e08c75b9a2
Lukasz Gryglicki <lgryglicki@cncf.io>
2026-02-27 13:01:26 +0100

    Renaming projects - wip 19
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>

--- util_sh/combined_projects_size_run.sh
+++ util_sh/combined_projects_size_run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./util_sh/combined_projects_size.sh 2016-03-10 "'Kubernetes', 'Prometheus', 'OpenTracing', 'Fluentd', 'Linkerd', 'gRPC', 'CoreDNS', 'containerd', 'rkt', 'CNI', 'Envoy', 'Jaeger', 'Notary', 'TUF', 'Rook', 'Vitess', 'NATS', 'OPA', 'SPIFFE', 'SPIRE', 'CloudEvents', 'Telepresence', 'Helm', 'OpenMetrics', 'Harbor', 'TiKV', 'Cortex', 'Buildpacks', 'Falco'"
+./util_sh/combined_projects_size.sh 2016-03-10 "'Kubernetes', 'Prometheus', 'OpenTracing', 'Fluentd', 'Linkerd', 'gRPC', 'CoreDNS', 'containerd', 'rkt', 'CNI', 'Envoy', 'Jaeger', 'Notary', 'TUF', 'Rook', 'Vitess', 'NATS', 'Open Policy Agent', 'SPIFFE', 'SPIRE', 'CloudEvents', 'Telepresence', 'Helm', 'OpenMetrics', 'Harbor', 'TiKV', 'Cortex', 'Buildpacks', 'Falco'"
