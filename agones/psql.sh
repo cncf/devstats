2d86697a4306513a312b71c30d1aded00bd7696f
Lukasz Gryglicki <lgryglicki@cncf.io>
2026-03-16 10:23:08 +0100

    Adding Agones - new CNCF Sandbox project 18
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>

--- agones/psql.sh
+++ agones/psql.sh
@@ -15,7 +15,7 @@ GHA2DB_PROJECT=agones PG_DB=agones GHA2DB_LOCAL=1 structure 2>>errors.txt | tee 
 ./devel/db.sh psql agones -c "create extension if not exists pgcrypto" || exit 1
 ./devel/db.sh psql agones -c "create extension if not exists hll" || exit 1
 ./devel/ro_user_grants.sh agones || exit 2
-GHA2DB_PROJECT=agones PG_DB=agones GHA2DB_LOCAL=1 gha2db 2018-03-05 0 today now 'agones-dev,googleforgames/agones,googleforgames/agones-site,googleforgames/agones-community,googleforgames/space-agon,googleforgames/global-multiplayer-demo,GoogleClundPlatform/agones' 2>>errors.txt | tee -a run.log || exit 3
+GHA2DB_PROJECT=agones PG_DB=agones GHA2DB_LOCAL=1 gha2db 2018-03-05 0 today now 'agones-dev,googleforgames/agones,googleforgames/agones-site,googleforgames/agones-community,googleforgames/space-agon,googleforgames/global-multiplayer-demo,GoogleCloudPlatform/agones' 2>>errors.txt | tee -a run.log || exit 3
 GHA2DB_PROJECT=agones PG_DB=agones GHA2DB_LOCAL=1 GHA2DB_MGETC=y GHA2DB_SKIPTABLE=1 GHA2DB_INDEX=1 structure 2>>errors.txt | tee -a run.log || exit 5
 GHA2DB_PROJECT=agones PG_DB=agones ./shared/setup_repo_groups.sh 2>>errors.txt | tee -a run.log || exit 6
 GHA2DB_PROJECT=agones PG_DB=agones ./shared/setup_scripts.sh 2>>errors.txt | tee -a run.log || exit 7
