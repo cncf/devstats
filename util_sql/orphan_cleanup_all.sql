d38b78c918b621db811975e1b64b342f9e861a53
Łukasz Gryglicki <lgryglicki@cncf.io>
2026-07-08 13:06:27 +0200

    Orphan checks scripts 2
    Signed-off-by: Łukasz Gryglicki <lgryglicki@cncf.io>

--- util_sql/orphan_cleanup_all.sql
+++ util_sql/orphan_cleanup_all.sql
@@ -1,10 +1,19 @@
--- Wipes ALL orphan-restored (origin=2) data from a DB, for a clean rerun with a fixed binary.
+-- Wipes ALL orphan-restored data from a DB, for a clean rerun with a fixed binary.
 begin;
 
-create temporary table tmp_orphan_events on commit drop as
+create temporary table tmp_orphan_events(event_id bigint primary key) on commit drop;
+
+insert into tmp_orphan_events
 select distinct event_id
 from gha_commits
-where origin = 2;
+where origin = 2
+on conflict do nothing;
+
+insert into tmp_orphan_events
+select distinct event_id
+from gha_payloads
+where action = 'restored_orphan_commit'
+on conflict do nothing;
 
 select count(*) as events_to_delete from tmp_orphan_events;
 
@@ -20,8 +29,10 @@ delete from gha_payloads p
 using tmp_orphan_events e
 where p.event_id = e.event_id;
 
-delete from gha_commits
-where origin = 2;
+delete from gha_commits c
+using tmp_orphan_events e
+where c.event_id = e.event_id
+  and c.origin = 2;
 
 delete from gha_events ge
 using tmp_orphan_events e
